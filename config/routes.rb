cbc199d7d1feaf357d3c7e06bea121234883336e
bwelds <bwelds@gmail.com>
2012-11-14 20:31:05 -0500

    Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RedditOnRails::Application.routes.draw do
+  
+  
+
+  
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -12,6 +18,8 @@ RedditOnRails::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :pages
 
   # Sample resource route with options:
   #   resources :products do
@@ -49,7 +57,7 @@ RedditOnRails::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'pages#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
