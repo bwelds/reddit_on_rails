2b3e5667e913b2bb95a22880d03945aa721ea14b
bwelds <bwelds@gmail.com>
2012-11-19 12:26:27 -0500

    Added username to sign up, allow usrs to sign in with username or email and displayed username on Link show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,30 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
-  attr_accessible :email, :username
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login
 
   has_many :links
   has_many :comments
   has_many :votes
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+
+### This is the correct method you override with the code above
+### def self.find_for_database_authentication(warden_conditions)
+### end 
+
 end
